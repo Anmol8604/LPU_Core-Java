98cf5085450d45374323ec9c1f913fe1643f6b68
Anmol8604 <anmol.12018604@lpu.in>
2023-01-12 22:57:39 +0530

    Lecture 1

---
+++ Comments.java
@@ -0,0 +1,11 @@
+public class Comments {
+    // If you want to create a java executable file, you should have a main method
+    /**
+     * mutli
+     * line
+     * comment
+     */
+    public static void main(String[] args) {
+        int a = 100;
+    }
+}
